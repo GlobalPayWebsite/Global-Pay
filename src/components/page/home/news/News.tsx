b631e01d87d79eec1ea912c5a93379162645e533
Nikhil Rai <nikhilrai.uptechsys@gmail.com>
2024-11-03 21:07:44 -0800

    add all service data in news

--- src/components/page/home/news/News.tsx
+++ src/components/page/home/news/News.tsx
@@ -2,8 +2,7 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import NewsCard from './NewsCard';
-import { DetailType } from '@/typing';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 import { motion } from 'framer-motion';
 
 // Import Swiper styles
@@ -11,89 +10,8 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 import './news.css'
+import { newsList } from '@/utils/data/news/newsData';
 
-export const newsList: DetailType[] = [
-    {
-        _id: '1',
-        title: 'Job Placement Assistance',
-        title_jp: '', // Add Japanese translation if available
-        category: 'Job',
-        category_jp: '', // Add Japanese translation if available
-        description: 'We specialize in helping individuals residing in Japan find suitable job opportunities. Our team works closely with employers to match candidates with positions that align with their skills and career goals.',
-        description_jp: '', // Add Japanese translation if available
-        extraDescription: '', // Add extra description if necessary
-        extraDescription_jp: '', // Add extra Japanese description if necessary
-        image: '/assets/jobplacement.jpg',
-        extraImage: '', // Add extra image if available
-        _createdAt: 'October 9, 2024',
-        icon: <i className="fa-solid fa-briefcase" />,
-        isNews: true
-    },
-    {
-        _id: '2',
-        title: 'Support for Nepali Graduates',
-        title_jp: '', // Add Japanese translation if available
-        category: 'International Students',
-        category_jp: '', // Add Japanese translation if available
-        description: 'We are dedicated to assisting Nepali individuals holding a bachelor’s degree who aspire to work in Japan.',
-        description_jp: '', // Add Japanese translation if available
-        extraDescription: '', // Add extra description if necessary
-        extraDescription_jp: '', // Add extra Japanese description if necessary
-        image: '/assets/nepali-students.jpg',
-        extraImage: '', // Add extra image if available
-        _createdAt: 'October 10, 2024',
-        icon: <i className="fa-solid fa-graduation-cap" />,
-        isNews: true
-    },
-    {
-        _id: '3',
-        title: 'Personalized Career Counseling',
-        title_jp: '', // Add Japanese translation if available
-        category: 'Career',
-        category_jp: '', // Add Japanese translation if available
-        description: 'Our experienced counselors offer personalized career advice and support, helping candidates understand the job market, refine their resumes, and prepare for interviews.',
-        description_jp: '', // Add Japanese translation if available
-        extraDescription: '', // Add extra description if necessary
-        extraDescription_jp: '', // Add extra Japanese description if necessary
-        image: '/assets/personalized-career.jpg',
-        extraImage: '', // Add extra image if available
-        _createdAt: 'October 11, 2024',
-        icon: <i className="fa-solid fa-user-tie" />,
-        isNews: true
-    },
-    {
-        _id: '4',
-        title: 'Employer Partnerships',
-        title_jp: '', // Add Japanese translation if available
-        category: 'Employer',
-        category_jp: '', // Add Japanese translation if available
-        description: 'We have established strong partnerships with employers across industries in Japan, allowing us to offer candidates exclusive job opportunities.',
-        description_jp: '', // Add Japanese translation if available
-        extraDescription: '', // Add extra description if necessary
-        extraDescription_jp: '', // Add extra Japanese description if necessary
-        image: '/assets/partnership.jpg',
-        extraImage: '', // Add extra image if available
-        _createdAt: 'October 13, 2024',
-        icon: <i className="fa-regular fa-handshake" />,
-        isNews: true
-    },
-    {
-        _id: '5',
-        title: 'Employer Partnerships',
-        title_jp: '', // Add Japanese translation if available
-        category: 'Employer',
-        category_jp: '', // Add Japanese translation if available
-        description: 'We have established strong partnerships with employers across industries in Japan, allowing us to offer candidates exclusive job opportunities.',
-        description_jp: '', // Add Japanese translation if available
-        extraDescription: '', // Add extra description if necessary
-        extraDescription_jp: '', // Add extra Japanese description if necessary
-        image: '/assets/partnership.jpg',
-        extraImage: '', // Add extra image if available
-        _createdAt: 'October 14, 2024',
-        icon: <i className="fa-regular fa-handshake" />,
-        isNews: true
-    },
-];
 
 
 const News: React.FC = () => {
@@ -119,7 +37,7 @@ const News: React.FC = () => {
                 transition={{ duration: 0.7, delay: 0.2 }}
             >
                 <Swiper
-                    modules={[Pagination, Navigation]}
+                    modules={[Pagination, Navigation,Autoplay]}
                     spaceBetween={20} // Space between slides
                     slidesPerView={1} // Initially, show one slide
                     breakpoints={{
@@ -141,6 +59,10 @@ const News: React.FC = () => {
                     }}
                     navigation={true}
                     loop={true}
+                    autoplay={{
+                        delay: 3000, // 3 seconds
+                        disableOnInteraction: false,
+                    }}
                     className="w-full pb-20"
                 >
                     {newsList.map((details, index) => (
